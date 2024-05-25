40cfc5656116f4988f3fd6281d88b5701f8e8e98
noorhanowaisi <1200983@student.birzeit.edu>
2024-05-25 13:56:29 +0300

    api speech to text (test arabic)

--- src/main/java/com/example/convoconvert/Controller/SpeachToTextApi.java
+++ src/main/java/com/example/convoconvert/Controller/SpeachToTextApi.java
@@ -1,44 +1,25 @@
 package com.example.convoconvert.Controller;
 
 import com.example.convoconvert.Service.SpeachToTextService;
-import kong.unirest.HttpResponse;
-import kong.unirest.Unirest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import java.io.File;
+
 import java.io.IOException;
 
 @RestController
 public class SpeachToTextApi {
 
+    @Autowired
+    private SpeachToTextService speachToTextService;
 
-//    RestTemplate restTemplate = new RestTemplate();
-//
-//    private static final String url = "";
-//
-//    @Bean
-//    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-//        return builder.build();
-//    }
-//
-//    @Bean
-//    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-//        return args -> {
-//
-//
-//        };
-//
-//    }
-@Autowired
-private SpeachToTextService speachToTextService;
-//    @GetMapping("/transcribe")
-//    public String transcribe(@RequestParam String filePath) {
-//        return speachToTextService.createTranscriptionJob(filePath);
-//    }
-    @PostMapping("transcribe")
-    public void makePostRequest(){
-        speachToTextService.makePostRequest();
+    @PostMapping("/transcribe")
+    public ResponseEntity<String> convertSpeechToText(@RequestParam("file") MultipartFile file) throws IOException {
+        if (file.isEmpty()) {
+            return new ResponseEntity<>("Please select a file to upload.", HttpStatus.BAD_REQUEST);
+        }
+        return speachToTextService.convertSpeechToText(file);
     }
 }
--- src/main/java/com/example/convoconvert/Service/SpeachToTextService.java
+++ src/main/java/com/example/convoconvert/Service/SpeachToTextService.java
@@ -1,70 +1,80 @@
 package com.example.convoconvert.Service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
+import java.io.IOException;
+
 @Service
-public class SpeachToTextService{
+public class SpeachToTextService {
+
+    private final RestTemplate restTemplate;
 
     @Autowired
-    private RestTemplate restTemplate;
+    public SpeachToTextService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
-//    public String createTranscriptionJob(String filePath) {
-//        try {
-//            String url = "https://asr.api.speechmatics.com/v2/jobs/";
-//
-//            HttpHeaders headers = new HttpHeaders();
-//            headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-//            headers.set("Authorization", "Bearer 2ffRxB9aEbHd5LFmIRZwiH0iuGNoRcpQ");
-//
-//            String config = "{\"type\": \"transcription\",\"transcription_config\": { \"operating_point\":\"enhanced\", \"language\": \"en\", \"enable_entities\": true}}";
-//
-//            MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
-//            body.add("config", config);
-//            body.add("file", new FileSystemResource(new File(filePath)));
-//
-//            HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
-//
-//            ResponseEntity<String> response = restTemplate.postForEntity(url, requestEntity, String.class);
-//
-//            return response.getBody();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            return "{\"error\": \"An error occurred while processing the transcription job\"}";
-//        }
-//    }
-
-    public void makePostRequest() {
-        RestTemplate restTemplate = new RestTemplate();
-
+    public ResponseEntity<String> convertSpeechToText(MultipartFile file) throws IOException {
         HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer 2ffRxB9aEbHd5LFmIRZwiH0iuGNoRcpQ");
+        headers.set("Authorization", "Bearer UGQ5b3MLYxEE5aue9xZ7OUTMiVR8Athq");
         headers.setContentType(MediaType.MULTIPART_FORM_DATA);
 
         MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
-        body.add("config", "{\"type\": \"transcription\",\"transcription_config\": { \"operating_point\":\"enhanced\", \"language\": \"en\", \"enable_entities\": true}}");
-
-        // Make sure the path points to the correct location of your file
-        Resource fileResource = new FileSystemResource("C:\\Users\\Lenovo\\Downloads\\example.wav");
-        body.add("data_file", fileResource);
+        body.add("config", "{\"type\": \"transcription\",\"transcription_config\": { \"operating_point\":\"enhanced\", \"language\": \"ar\", \"enable_entities\": true}}");
+        body.add("data_file", new ByteArrayResource(file.getBytes()) {
+            @Override
+            public String getFilename() {
+                return file.getOriginalFilename();
+            }
+        });
 
         HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
 
-        ResponseEntity<String> response = restTemplate.exchange(
+        ResponseEntity<String> response= restTemplate.exchange(
                 "https://asr.api.speechmatics.com/v2/jobs/",
                 HttpMethod.POST,
                 requestEntity,
                 String.class
         );
+        String responseBody = response.getBody();
+        JsonNode jsonNode = new ObjectMapper().readTree(responseBody);
+        String jobid = jsonNode.get("id").asText();
+        System.out.println("id: "+jobid);
+        HttpHeaders transcriptHeaders = new HttpHeaders();
+        transcriptHeaders.set("X-SM-EAR-Tag", "<string>");
+        transcriptHeaders.set("Authorization", "Bearer UGQ5b3MLYxEE5aue9xZ7OUTMiVR8Athq");
+        transcriptHeaders.set("Accept", "application/json");
+        HttpEntity<String> transcriptRequestEntity = new HttpEntity<>(transcriptHeaders);
+        ResponseEntity<String> jobDetails;
+        String jobStatus="running";
+        while (jobStatus.equals("running")){
+            jobDetails= restTemplate.exchange(
+                    "https://asr.api.speechmatics.com/v2/jobs/"+jobid,
+                    HttpMethod.GET,
+                    transcriptRequestEntity,
+                    String.class
+            );
+            JsonNode jsonNode2 = new ObjectMapper().readTree(jobDetails.getBody());
+            jobStatus = jsonNode2.get("job").get("status").asText();
+            System.out.println("status: "+jobStatus);
+        }
+        return restTemplate.exchange(
+                "https://asr.api.speechmatics.com/v2/jobs/"+jobid+"/transcript?format=txt",
+                HttpMethod.GET,
+                transcriptRequestEntity,
+                String.class
+        );
 
-        System.out.println("Response: " + response.getBody());
     }
+
 
 }

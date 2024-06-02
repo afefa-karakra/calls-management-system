b5a8c653df0490ad0425453edeb8c9271ffdbe7e
noorhanowaisi <1200983@student.birzeit.edu>
2024-06-02 13:33:32 +0300

    Wojood api is work

--- src/main/java/com/example/convoconvert/Controller/SpeachToTextApi.java
+++ src/main/java/com/example/convoconvert/Controller/SpeachToTextApi.java
@@ -1,7 +1,9 @@
 package com.example.convoconvert.Controller;
 
 import com.example.convoconvert.Service.SpeachToTextService;
+import kong.unirest.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
